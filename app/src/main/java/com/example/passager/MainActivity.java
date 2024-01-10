7f2a2e00a2eb99c610ab37b69014ba2f9727728f
rafail <rafailkalenderis@yahoo.com>
2024-01-10 07:48:36 +0100

    Final

--- app/src/main/java/com/example/passager/MainActivity.java
+++ app/src/main/java/com/example/passager/MainActivity.java
@@ -22,7 +22,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -61,7 +60,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
